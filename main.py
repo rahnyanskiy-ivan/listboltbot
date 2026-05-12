c146cf7883b56f62295bb8037ea560a1712bdde7
rahnyanskiy-ivan <71345596+rahnyanskiy-ivan@users.noreply.github.com>
2026-05-13 00:03:44 +0300

    main.py

--- main.py
+++ main.py
@@ -18,12 +18,16 @@ from aiogram.types import (
 # CONFIG
 # =========================
 
-TOKEN = os.getenv("8205588708:AAF-cMpq38G-gR4xsdogcOw3h0iBoH3Zcck")
-GROUP_ID = int(os.getenv"-1003949873404"))
-ADMIN_PASSWORD = os.getenv("admin123")
+TOKEN = os.getenv("TOKEN")
 
 bot = Bot(token=TOKEN)
-await dp = dp.start_poling(bot)
+
+dp = Dispatcher(storage=MemoryStorage())
+
+async def main():
+    print("Bot started")
+    await dp.start_polling(bot)
+
 
 # =========================
 # DATA
